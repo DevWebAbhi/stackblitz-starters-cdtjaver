0c7590fb3b08c7783cae5a42e60a8ff7638c06b1
DevWebAbhi <tiwariabhishek889912@gmail.com>
2025-05-01 13:37:31 +0530

    DONE

--- index.js
+++ index.js
@@ -1,15 +1,58 @@
-const express = require('express');
-const { resolve } = require('path');
-
+const express = require("express");
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const dotenv = require("dotenv");
+dotenv.config();
 const app = express();
-const port = 3010;
+app.use(express.json());
 
-app.use(express.static('static'));
 
-app.get('/', (req, res) => {
-  res.sendFile(resolve(__dirname, 'pages/index.html'));
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log("MongoDB Connected"))
+.catch((err) => console.error("MongoDB connection error:", err));
+
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+const User = mongoose.model("User", userSchema);
+
+
+app.post("/register", async (req, res) => {
+  const { username, email, password } = req.body;
+
+  // Check for empty fields
+  if (!username || !email || !password) {
+    return res.status(400).json({ message: "All fields are required." });
+  }
+
+  try {
+   
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = new User({
+      username,
+      email,
+      password: hashedPassword,
+    });
+
+
+    await newUser.save();
+
+    res.status(201).json({ message: "User registered successfully." });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error." });
+  }
+});
+
+
+app.listen(3000, () => {
+  console.log("Server is running on http://localhost:3000");
 });
